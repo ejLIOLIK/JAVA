fdf0d2bf0078816baadfb5bed5eae2c269468dc8
KEJ <118498775+ejLIOLIK@users.noreply.github.com>
2022-12-01 17:10:53 +0900

    Add files via upload

---
+++ Kiosk/src/kiosk/catcafe/Product.java
@@ -0,0 +1,17 @@
+package kiosk.catcafe;
+
+public class Product {
+
+	String name;
+	int price;
+	int order=0;
+	
+	Product(String name, int price){
+		this.name = name;
+		this.price = price;
+	}
+	
+	public void info() {
+		System.out.println(name + " " + price +"원");
+	}
+}
