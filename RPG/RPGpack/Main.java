70d99c68a4b989e17e8e6629d4a630732f339912
KEJ <118498775+ejLIOLIK@users.noreply.github.com>
2022-12-02 16:35:27 +0900

    Add files via upload

--- RPG/RPGpack/Main.java
+++ RPG/RPGpack/Main.java
@@ -7,15 +7,28 @@ public class Main {
 	public static void main(String[] args) {
 
 		Character elf = new Character("레골라스", 100, 20, 1000);
-		Monster orc = new Monster("오크족장", 50, 30, 100, 10);
+		Monster orc = new Monster("오크족장", 50, 30, 100, 10, 1006);
 		Scanner sc = new Scanner(System.in);
 		boolean bat = false; // 전투 true 전투 종료 false
 		int turn = 1; // 턴 카운트
 		//int tempId = elf.id;
 		
+		Monster mob[] = new Monster[] {new Monster("허수아비1", 30, 0, 50, 0, 0),
+				new Monster("허수아비2", 30, 0, 50, 0, 0),
+				new Monster("허수아비3", 30, 0, 50, 0, 0),
+				new Monster("쥐1", 50, 15, 80, 10, 0),
+				new Monster("쥐3", 50, 15, 80, 10, 0),
+				new Monster("왕쥐", 100, 30, 120, 50, 0),
+				};
+		
 		Room map[] = new Room[]{new Room("연습장 입구","연습장으로 들어가는 입구다.",1000,1001,0,0,0,0,0),
 				new Room("연습장 서쪽","연습장 서쪽이다.",1001,1002,1000,0,0,0,0),
-				new Room("연습장 중앙","연습장 중앙이다.",1002,0,1001,0,0,0,0)};
+				new Room("연습장 중앙","연습장 중앙이다.",1002,0,1001,1004,1003,1005,1006),
+				new Room("연습장 북쪽","연습장 북쪽이다.",1003,0,0,1002,0,0,0),
+				new Room("연습장 남쪽","연습장 남쪽이다.",1004,0,0,0,1002,0,0),
+				new Room("연습장 누각","연습장 누각이다.",1005,0,0,0,0,0,1002),
+				new Room("연습장 지하","연습장 지하다.",1006,0,0,0,0,1002,0),
+				};
 		
 		elf.info();
 		System.out.println();
--- RPG/RPGpack/Monster.java
+++ RPG/RPGpack/Monster.java
@@ -7,19 +7,22 @@ public class Monster {
 	int fullHp;
 	int exp;
 	int gold;
-	boolean life=true;
+	boolean life;
+	int id;
 	
 	void info() {
 		System.out.println(this.name + "[" +this.hp+ "/" +this.fullHp+ "]");
 	}
 	
 	//생성자 함수
-	Monster(String n, int h, int a, int e, int g){
+	Monster(String n, int h, int a, int e, int g, int id){
 		this.name = n;
 		this.hp = h;
 		this.fullHp = h;
 		this.attack = a;
 		this.exp = e;
 		this.gold = g;
+		this.life=true;
+		this.id=id;
 	}
 }
